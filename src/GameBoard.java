2fe9b6c02096f9873d927d5c8310f1530e36302e
Bernard Wong <bew030@ucsd.edu>
2018-12-17 01:02:06 -0800

    rectangle object, start of gameboard
    finished the rectangle class, which is each rectangle obect in the grid.
    also finished the start of the gameboard, where 2 rectangles are randomly generated. Might need to change up the coordinate system based off the way the items are shifted

---
+++ src/GameBoard.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class GameBoard {
+    private Rectangle[][] overallGameBoard;
+    private ArrayList<int[]> unoccupiedCoordinates;
+    private int[] possibleGeneratedValue = new int[]{2,4};
+    private Random rand = new Random();
+
+    public GameBoard() {
+        unoccupiedCoordinates = new ArrayList<int[]>();
+        overallGameBoard = new Rectangle[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                overallGameBoard[i][j] = null;
+                unoccupiedCoordinates.add(new int[]{i,j});
+            }
+        }
+        Rectangle firstRect = new Rectangle();
+        Rectangle secondRect = new Rectangle();
+
+        // always starts off with two random rectangles of either value 2 or 4
+        int firstRectVal = possibleGeneratedValue[rand.nextInt(2)];
+        int secondRectVal = possibleGeneratedValue[rand.nextInt(2)];
+        firstRect.setValue(firstRectVal);
+        secondRect.setValue(secondRectVal);
+
+        // two random locations for the two rectangles
+        int[] firstLoc = randomCoordinateGenerator();
+        firstRect.setxCoord(firstLoc[0]);
+        firstRect.setyCoord(firstLoc[1]);
+        unoccupiedCoordinates.remove(firstLoc);
+
+        int[] secLoc = randomCoordinateGenerator();
+        secondRect.setxCoord(secLoc[0]);
+        secondRect.setyCoord(secLoc[1]);
+        unoccupiedCoordinates.remove(secLoc);
+
+        // sets onto board
+        overallGameBoard[firstRect.getxCoord()][firstRect.getyCoord()] = firstRect;
+        overallGameBoard[secondRect.getxCoord()][secondRect.getyCoord()] = secondRect;
+    }
+
+    public int[] randomCoordinateGenerator() {
+        int n = rand.nextInt(unoccupiedCoordinates.size());
+        return unoccupiedCoordinates.get(n);
+    }
+
+    public void printViewer() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(overallGameBoard[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        GameBoard gameTester = new GameBoard();
+        gameTester.printViewer();
+    }
+}
---
+++ src/Rectangle.java
@@ -0,0 +1,49 @@
+public class Rectangle {
+
+    private int intValue;
+    private int xCoord;
+    private int yCoord;
+    /**
+     * Rectangle default constructor.
+     */
+    public Rectangle() {
+        intValue = 0;
+    }
+
+    public void setValue(int addOn) {
+        intValue+= addOn;
+    }
+
+    public int getValue() {
+        return intValue;
+    }
+
+    public void setxCoord(int x) {
+        xCoord = x;
+    }
+
+    public int getxCoord() {
+        return xCoord;
+    }
+
+    public void setyCoord(int y) {
+        yCoord = y;
+    }
+
+    public int getyCoord() {
+         return yCoord;
+    }
+
+    public String toString() {
+        if (this.equals(null)) {
+            return null;
+        }
+        return Integer.toString(intValue);
+    }
+
+    public static void main(String[] args) {
+        Rectangle tester = new Rectangle();
+        tester.setValue(2);
+        System.out.println(tester.getValue());
+    }
+}
